482e8da8674136ba2d4e74eef43d2ae7fd868457
BlazeShotS <abelpaulo.durandpalacios21@gmail.com>
2025-12-08 10:13:45 -0500

    Initial commit: Subiendo proyecto

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,12 @@
+<div>
+    <!-- Favicon Icon -->
+    <link rel="icon" href="assets/img/favicon.png">
+    <!-- Site Title -->
+    <title>Arino - Creative Agency Template</title>
+    <link rel="stylesheet" href="assets/css/plugins/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/fontawesome.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/slick.css">
+    <link rel="stylesheet" href="assets/css/plugins/lightgallery.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/animate.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</div>
---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
+
+
+Route::get('/', [PagesController::class, 'Inicio'] ); //Nombre del metodo del controlador, Inicio
+
+
