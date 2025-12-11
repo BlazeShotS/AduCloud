c1b133088d65fc4e5bf19e07c25b2b059b7185a4
BlazeShotS <abelpaulo.durandpalacios21@gmail.com>
2025-12-10 19:39:46 -0500

    style: Agregacion del section de mision y services

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -9,4 +9,51 @@
             <a href="#" class="hero-btn">Solicitar Cotización</a>
         </div>
     </section>
+
+    <section class="informacion-empresa">
+        <img src="{{ asset('img/portfolio_38.jpeg') }}" alt="Imagen educación tecnológica" class="empresa-img">
+        <div class="empresa-texto">
+            <h1>Acerca de nuestros servicios y soluciones tecnológicas innovadoras de TI</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quia facere praesentium,
+                quos omnis unde at sit nihil reiciendis ipsum sunt quam suscipit.
+            </p>
+        </div>
+    </section>
+
+    <section class="especializados">
+        <div class="titulo-servicios">
+            <h2>Servicios especializados</h2>
+            <p>Especialidad</p>
+        </div>
+
+        <div class="cards-servicios">
+
+            <div class="card">
+                <img src="https://picsum.photos/330/260?random=1">
+                <span class="badge"></span>
+                <p class="card-title">UI / UX Design</p>
+            </div>
+
+            <div class="card">
+                <img src="https://picsum.photos/330/260?random=2">
+                <span class="badge"></span>
+                <p class="card-title">React.js Development</p>
+            </div>
+
+            <div class="card">
+                <img src="https://picsum.photos/330/260?random=3">
+                <span class="badge"></span>
+                <p class="card-title">Digital Marketing</p>
+            </div>
+
+            <div class="card">
+                <img src="https://picsum.photos/330/260?random=4">
+                <span class="badge"></span>
+                <p class="card-title">Technology</p>
+            </div>
+
+        </div>
+    </section>
+
 </x-layout>
